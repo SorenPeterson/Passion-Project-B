e728f668db1dbdf5b16f30cdc389a50eaa89d0a5
Soren Peterson <soren468@gmail.com>
2015-02-13 14:49:51 -0600

    In application.js: create board and cell constructors. Board populates with zero values

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,39 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+var board;
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+function Cell(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+function Board() {
+  // Set size of board
+  this.size = 4;
+
+  // Populated board with empty cells
+  this.cells = []
+  for(var i = 0; i < this.size; i++) {
+    this.cells[i] = []
+    for(var j = 0; j < this.size; j++) {
+      this.cells[i][j] = 0;
+    }
+  }
+}
+
+Board.prototype.placeNewNumber = function() {
+  // Gather all locations of empty cells
+}
+
+Board.prototype.up = function() {
+
+}
+
+Board.prototype.down = function() {
+
+}
+
+
+
+$(document).ready(function() {
+  console.log('creating le board');
+  board = new Board();
 });
