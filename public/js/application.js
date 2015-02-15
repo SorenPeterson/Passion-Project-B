728916758d763265d43f4f0390ce3dd5a0fff039
Soren Peterson <soren468@gmail.com>
2015-02-14 18:46:52 -0600

    Update styling for game and cells to display cells correctly. Fix cloning of cell template in cell constructor. Add display method for cell objects. add html attribute for board. Board constructor automatically display all cells. Add cell template to index view

--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,25 @@
+var BOARD_SIZE = 400;
+var CELL_SIZE = BOARD_SIZE/4;
+
 var board;
 
 function Cell(x, y, value) {
   this.x = x;
   this.y = y;
   this.value = value;
+  this.html = $("#cell-template").find(".cell").clone();
+}
+
+Cell.prototype.display = function() {
+  this.html.css("top", this.y * CELL_SIZE)
+  this.html.css("left", this.x * CELL_SIZE)
 }
 
 function Board() {
   // Set size of board
   this.size = 4;
+
+  this.html = $("#game");
 
   // Populated board with empty cells
   this.cells = []
@@ -16,6 +27,8 @@ function Board() {
     this.cells[y] = []
     for(var x = 0; x < this.size; x++) {
       this.cells[y][x] = new Cell(x, y, 0);
+      this.html.append(this.cells[y][x].html);
+      this.cells[y][x].display();
     }
   }
 }
